b8eaf399d1f53d867f2ecb290f590945c77aa346
sayed-kassem <sayed.webdev@gmail.com>
2023-01-04 13:02:53 +0200

    updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,25 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-    root: true,
-    extends: [
-        "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/eslint-config-prettier",
-    ],
-    overrides: [{
-        files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
-        extends: ["plugin:cypress/recommended"],
-    }, ],
-    parserOptions: {
-        ecmaVersion: "latest",
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  overrides: [
+    {
+      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
     },
-    env: {
-        node: true,
-    },
-    rules: {
-        "vue/multi-word-component-names": "off",
-    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
 };
