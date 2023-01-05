0a4203b5269edfbdb26ce40770ea0aa434c87e77
sayed-kassem <sayed.webdev@gmail.com>
2023-01-05 11:29:35 +0200

    updated

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -27,7 +27,7 @@
             <!-- Modal Close Button -->
             <div
               class="modal-close cursor-pointer z-50"
-              @click="isOpen = false"
+              @click="modalVisibility = false"
             >
               <i class="fas fa-times"></i>
             </div>
@@ -37,20 +37,32 @@
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-white text-white bg-blue-600"
+                class="block rounded py-3 px-4 transition"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register',
+                }"
                 href="#"
+                @click.prevent="tab = 'login'"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" href="#"
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login',
+                }"
+                @click.prevent="tab = 'register'"
                 >Register</a
               >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -77,7 +89,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -158,9 +170,16 @@ import { mapState, mapWritableState } from "pinia";
 import useModalStore from "@/stores/modal";
 export default {
   name: "Auth",
+  data() {
+    return {
+      tab: "login",
+    };
+  },
   computed: {
     ...mapState(useModalStore, ["hiddenClass"]),
-    ...mapWritableState(useModalStore, ["isOpen"]),
+    ...mapWritableState(useModalStore, {
+      modalVisibility: "isOpen",
+    }),
   },
 };
 </script>
