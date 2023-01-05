30f1dd5ec2a7cb6aca09bd27ece1275e0e2bb937
sayed-kassem <sayed.webdev@gmail.com>
2023-01-05 21:27:40 +0200

    updated

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -89,15 +89,17 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
+                name="name"
                 type="text"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -159,7 +161,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -168,11 +170,21 @@
 <script>
 import { mapState, mapWritableState } from "pinia";
 import useModalStore from "@/stores/modal";
+import { ErrorMessage } from "vee-validate";
 export default {
   name: "Auth",
   data() {
     return {
       tab: "login",
+      schema: {
+        name: "required",
+        email: "",
+        age: "",
+        password: "",
+        confirm_password: "",
+        country: "",
+        tos: "",
+      },
     };
   },
   computed: {
@@ -181,5 +193,6 @@ export default {
       modalVisibility: "isOpen",
     }),
   },
+  components: { ErrorMessage },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import router from "./router";
 
 import "./assets/base.css";
 import "./assets/main-copy.css";
-
+import VeeValidatePlugin from "./includes/validation";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
