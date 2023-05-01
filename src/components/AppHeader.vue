fd38944b74d8520ac078e94c9deb44ea024ccd0a
sayed-kassem <sayed.webdev@gmail.com>
2023-05-01 11:32:15 +0300

    updated

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,13 +2,16 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#"
-        >Spotify</a
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{name: 'home'}" exact-active-class="no-active"
+        >Spotify</router-link
       >
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link  :to="{name: 'about'}" class="px-2 text-white">About</router-link>
+          </li>
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -16,7 +19,7 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link :to="{name: 'manage'}" class="px-2 text-white" >Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signOutUser">Logout</a>
@@ -32,16 +35,16 @@ import { mapStores } from "pinia";
 import useModalStore from "@/stores/modal";
 import useUserStore from "@/stores/user";
 export default {
-  name: "AppHeader",
-  computed: {
-    ...mapStores(useModalStore, useUserStore),
-  },
-  methods: {
-    toggleAuthModal() {
-      this.modalStore.isOpen = !this.modalStore.isOpen;
-      console.log(this.modalStore.isOpen);
+    name: "AppHeader",
+    computed: {
+        ...mapStores(useModalStore, useUserStore),
     },
-  },
+    methods: {
+        toggleAuthModal() {
+            this.modalStore.isOpen = !this.modalStore.isOpen;
+            console.log(this.modalStore.isOpen);
+        },
+    },
 };
 </script>
 <style></style>
