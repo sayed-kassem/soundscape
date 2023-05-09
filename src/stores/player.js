90c41cc970bf9292a85d107f3f57bbd360e7d7cd
sayed-kassem <sayed.webdev@gmail.com>
2023-05-09 11:57:44 +0300

    updated

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,9 +1,12 @@
 import { defineStore } from "pinia";
 import { Howl } from "howler";
+import helper from "@/includes/helper"
 export default defineStore("player", {
     state: () => ({
         current_song: {},
         sound: {},
+        seek: "00:00", //current position of audio
+        duration: "00:00",
     }),
     actions: {
         async newSong(song) {
@@ -14,6 +17,35 @@ export default defineStore("player", {
             });
             this.sound.play();
 
+            this.sound.on("play", () => {
+                requestAnimationFrame(this.progress)
+            })
+
+        },
+        async toggleAudio() {
+            if (!this.sound.playing) {
+                return;
+            }
+            if (this.sound.playing()) {
+                this.sound.pause();
+            } else {
+                this.sound.play();
+            }
+        },
+        progress() {
+            this.seek = helper.formatTime(this.sound.seek());
+            this.duration = helper.formatTime(this.sound.duration());
+            if (this.sound.playing()) {
+                requestAnimationFrame(this.progress)
+            }
         }
     },
+    getters: {
+        playing: (state) => {
+            if (state.sound.playing) {
+                return state.sound.playing()
+            }
+            return false;
+        }
+    }
 })
