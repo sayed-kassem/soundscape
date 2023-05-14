4f827af55d3518e456451a2f4af8849b621a2945
sayed-kassem <sayed.webdev@gmail.com>
2023-05-14 16:01:46 +0300

    updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="text-center flex justify-center items-center video-background">
+  <section class="text-center flex justify-center items-center">
     <!-- <div
         class="absolute inset-0 w-full h-full bg-contain introduction-bg"
         style="background-image: url(assets/img/about-bg.gif);"
       ></div> -->    
       <div class="absolute inset-0 sm:w-full sm:h-full introduction-bg">
-        <video class="absolute inset-0 w-full h-full object-cover" src="assets/img/1.mp4" autoplay muted loop playsinline></video>
+        <video class="absolute inset-0 w-full h-full object-fill" src="assets/img/1.mp4" autoplay muted loop playsinline></video>
       </div>
 
-      <div class="container mx-auto">
+      <div class="container mx-auto mt-48">
         <div>
         <p class="w-full md:w-8/12 mx-auto text-xl text-white">
           🎵 Introducing SoundScape: Unleash Your Music Experience! 🚀 
@@ -22,20 +22,5 @@
 </template>
 
 <style>
-body{
-  height: 100vh;
-}
-.video-background{
-  position: relative;
-  overflow: hidden;
-  height: 90vh;
-}
-.video-background video {
-  position: absolute;
-  top:0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  object-fit: cover;
-}
+
 </style>
